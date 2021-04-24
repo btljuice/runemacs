b836413b4add5882a0031842f2445906bbe4ff6e
Alexis Trudeau <atrudeau@hopper.com>
2021-04-24 11:07:00 -0400

    Part 3 completed: Add Hydra

--- init.el
+++ init.el
@@ -134,3 +134,14 @@
 (use-package evil-collection
   :after evil
   :config (evil-collection-init))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(rune/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale text"))
